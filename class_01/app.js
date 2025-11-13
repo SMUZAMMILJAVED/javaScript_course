5787e65149bd7cc1cca66598ca2118783a02d25b
S MuzammiL Javed <121771009+SMUZAMMILJAVED@users.noreply.github.com>
2025-11-13 23:27:36 +0500

    from chapter 1 to 11

---
+++ class_01/app.js
@@ -0,0 +1,20 @@
+//                           alert 
+// alert("hello!");
+//                        data types 
+// number 
+// 3 35 0.56 -34
+// string" 
+// text 66%$#@"
+// boolean
+//  true false
+                         // variables 
+// var a;
+// a=90;
+// alert(a);
+                  // underscore and camelCase 
+                //  var this_is_an_apple;
+                // var  thisIaAnApple;
+                // shorthand 
+// var a=90;
+// alert(a);
+
---
+++ class_02/app.js
@@ -0,0 +1,46 @@
+
+// math operators
+// var num1=2;
+// num2=84;
+// var result=num2 %num1;      //first num always biggest in remainder
+// alert(result);
+// inc and dec operator 
+// var a=9;
+// a++;
+// ++a;
+// --a;
+// a--;
+// alert(a++);
+// alert(a);
+// var num=30;
+// var num2= num++;
+// alert(num);
+                            //BODMAS
+// var num1=2;
+// num2=3;
+// var result=((num1 + num2 + 5 )*6) /2;      //first num always biggest in remainder
+// alert(result); 
+                               // concatination 
+//   var num=5;
+//   var text="5";
+
+// alert(num +6 + text);
+                                     // prompt 
+// var num=  +prompt("enter any number","6");
+// console.log(num +10);
+
+                               // comparision operators 
+// console.log(5===5);
+// console.log(5!==5);
+// console.log(5>6);
+// console.log(5<=4);
+                               //  if else 
+// var num = +prompt("enter any number","5");
+// if(num ===5){
+//     console.log("number is 5");
+                                    
+// }
+// else{
+//     console.log("not five!");
+    
+// }
