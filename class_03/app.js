295513318b56f83fd6e58a99ca3b7d84b534333f
S MuzammiL Javed <121771009+SMUZAMMILJAVED@users.noreply.github.com>
2025-11-24 01:39:46 +0500

    class_03

---
+++ class_03/app.js
@@ -0,0 +1,119 @@
+// alert("hello world!");
+                              //  prompt 
+
+// var a=prompt("enter your name?");
+// console.log(a);
+
+// var a=+prompt("enter any number?",9);
+// console.log(a +9);
+
+// var a=prompt("enter any number?",9);
+// a=Number(a);
+// console.log(a +9);
+
+// var a=+prompt("enter any number?",9);
+// parseInt(a);
+// console.log(a +9);
+
+
+                              //  comparisons operators 
+// console.log(4=="4");
+// console.log(4==="4");
+// console.log(4===4);
+// console.log(4!=="4");
+// console.log(4>4);
+// console.log(4<4);
+// console.log(4>=4);
+// console.log(4<=4);
+
+                                     // if statement 
+// santax 
+// if( cond){
+// //    statement  
+// }
+// var num=4;
+// if(num>=5){
+//     console.log("HEllO");
+
+// }else{
+//     console.log("Number is less than or 5");
+
+// }
+
+                           // check even odd program 
+
+
+// var num=+prompt("enter any number",2);
+// if(num%2===0){
+//     console.log(num +" is an Even Number!");
+
+// }else{
+//      console.log(num +" is an Odd Number!");
+
+// }
+
+                            //if  else if 
+
+// var day = "mon";
+// var day = prompt("enter day names","sat");
+// if (day === "sat") {
+//     console.log("Today is Saturaday");
+// } else if (day === "sun") {
+//     console.log("Today is Sunday");
+// } else if (day === "mon") {
+//     console.log("Today is Monday");
+// } else if (day === "Tuesday") {
+//     console.log("Today is Tueaday");
+// }else{
+//     console.log("invalid Day Input");
+    
+// }
+
+                          // AND OR Operators 
+
+// console.log(false && true);
+// console.log(false || true);
+// console.log(false && false);
+// console.log(false || false);
+// console.log(true && true);
+// console.log(true || true);
+
+                //  number check is divide by 3 or 5 program 
+
+// var num=+prompt("type anu number",15);
+// if(num%3===0 && num %5===0){
+// console.log("Both 3 and 5");
+
+// }
+// else if(num%3===0){
+// console.log("only 3");
+
+// }
+// else if(num %5===0){
+// console.log("only 5");
+
+// }
+// else{
+//     console.log("number not divide by 3 or 5 ");
+    
+// }
+
+                               // concept of AND OR 
+
+// console.log(false && "false");
+// console.log(true || "falsy");
+
+                           // truthy falsy values 
+// if(false){
+//     console.log("truthy");
+    
+// }else{
+//     console.log("falsy");
+    
+// }
+
+                      // concept with truthty and falsy values 
+                      
+// console.log("null" && "undefined");
+// console.log(true ||"falsy");
+
