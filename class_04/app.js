cbad48b0238e07d2eb65b753543bd1859afebc06
S MuzammiL Javed <121771009+SMUZAMMILJAVED@users.noreply.github.com>
2025-11-28 01:45:50 +0500

    nested if else , array , array methods, for loop

---
+++ class_04/app.js
@@ -0,0 +1,76 @@
+// var num=+prompt("enter your percentage?");
+
+// if(num>=0 && num<=100){
+// if(num>=80){
+//     console.log("A+1");
+    
+// }else if(num>=70){
+//     console.log("A");
+    
+// }else if(num>=60){
+//     console.log("B");
+    
+// }else if(num>=50){
+//     console.log("C");
+    
+// }else if(num>=50){
+//     console.log("D");
+    
+// }else{
+//     console.log("Fail!");
+    
+// }
+
+// }else{
+//     console.log("invalid percentage " + num);
+    
+// }
+// var arr=['HEllO',67,true,"bilal"];
+// arr[0]=5;
+// console.log(arr[3]);
+// var arr=['january','febuary','march'];
+// arr.push('april');
+// arr.pop();
+// arr.unshift('december');
+// arr.shift();
+// console.log(arr);
+// var arr=["jan",'feb','april' , 4, 'march'];
+// arr.splice(2,2);
+// arr.push('april');
+// console.log(arr);
+// var arr=["jan",'feb','april' , 4, 'march'];
+// var newArr=arr.slice(2);
+// console.log(newArr);
+// console.log(newArr.length);
+
+
+
+
+// loop 
+
+// for(var i=0;i<100;i++){
+// console.log("hello"+i);
+
+// }
+
+// var num=+prompt("enter any number?",2);
+// for(var i=1;i<=10;i++){
+// console.log(num + ' x ' + i + " = " + num*i);
+// }
+
+
+// var cities=['karachi','lahore'];
+// var city=prompt('enter your city name','karachi');
+// var found=false;
+// for(var i=0;i<cities.length;i++){
+//     if(city===cities[i]){
+//         console.log("Found!");
+//         found=true;
+//         break;
+        
+//     }
+// }
+// if(!found){
+//     console.log("not found!");
+    
+// }
