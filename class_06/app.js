2c71d9233dfe68c75447d0ea0a604cee91fa48f5
S MuzammiL Javed <121771009+SMUZAMMILJAVED@users.noreply.github.com>
2025-12-05 01:43:09 +0500

    chapter 20 to 28

---
+++ class_06/app.js
@@ -0,0 +1,97 @@
+
+                          // nested for loop 
+
+// var arr=[[1,2,3],['a','b','c']]
+// for(var i=0;i<arr.length;i++){
+//     for(var j=0;j<arr[i].length;j++){
+// console.log(arr[i][j]);
+
+//     }
+// }
+
+
+// for(var i=0;i<5;i++){
+//     var star=''
+//     for(var j=0;j<=i;j++){
+// star+=" *"
+//     }
+//     console.log(star);
+    
+// }
+
+// changing case 
+// var text='This is an Apple';
+// text = text.toUpperCase();
+// console.log(text);
+
+
+// var cities=['karachi','lahore'];
+// var city=prompt('enter city name',"Karachi");
+// var found=false;
+// for(var i=0;i<cities.length;i++){
+//     if(city.toLowerCase()===cities[i].toLowerCase()){
+//         console.log("found");
+//         found=true;
+//         break;
+//     }
+// }
+// if(!found){
+//     console.log("not found!");
+    
+// }
+
+              // measuring length and extracting parts from string 
+// var text='this is an apple';
+// console.log(text.length); //length 
+// console.log(text.slice(11,16)); //extract
+
+
+                        // finding segment 
+
+// var text='this is an apple';
+// console.log(text.indexOf("a"));
+
+// console.log(text.lastIndexOf('a'));
+
+                      // find character
+
+// var text='this is an apple';
+// console.log(text.charAt(text.length - 1));
+
+//replace 
+// var text='this is an apple';
+// text = text.replaceAll("a","new");
+// console.log(text);
+
+                 // round/floor/ceil numbers 
+
+// var num=7.9;
+// console.log(Math.round(num));
+// console.log(Math.floor(num));
+// console.log(Math.ceil(num));
+
+
+                  //generate random number
+// var num=Math.ceil(Math.random()*5);
+// console.log(num);
+
+// var num=Math.ceil(Math.random()*10);
+// var guess=+prompt("enter number between 1 to 10",num);
+// if(guess===num){
+//     console.log("win!");
+    
+// }
+// else if(num +1 ===guess || num -1 ===guess){
+// console.log("close number is",num);
+
+// }
+
+// else{
+//     console.log("loose! num is",num);
+    
+// }
+// convert string to number/decimal
+// var num ='23.9';
+// num=parseInt(num)
+// console.log(num);
+
