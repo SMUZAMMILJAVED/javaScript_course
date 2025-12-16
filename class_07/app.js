3fe29026cc66be8f92b977eb8f4d6e1c358b95ab
S MuzammiL Javed <121771009+SMUZAMMILJAVED@users.noreply.github.com>
2025-12-17 02:11:11 +0500

    from chapter 41 chapter 50

---
+++ class_07/app.js
@@ -0,0 +1,61 @@
+                                             // while loop 
+// var i=5;
+// while(i>0){
+// console.log(i);
+// i--
+// }
+// console.log("hello");
+
+
+// var correctPin='123';
+// var userInput=prompt("enter your pin",123);
+// while(userInput!==correctPin){
+// userInput=prompt("wrong pin");
+// }
+// console.log("correct!");
+
+
+                                     // do while 
+// var i=10;
+
+// do{
+//     console.log(i);
+    
+// i++;
+// }while(i<5)
+
+//
+// console.log("hello!");
+
+                                         // events 
+
+
+// function myFunc(val){
+//     console.log(val);
+    
+// }
+// function getVal(){
+//     var input=document.getElementById('p_1');
+//     console.log(input.value);
+    
+// }
+// function setVal(){
+//     var input=document.getElementById('p_1');
+//    input.value="hello world!"
+    
+// }
+// function bgChange(){
+//      var input=document.getElementById('bg');
+//      input.style.backgroundColor='red';
+
+// }
+// function bgWhite(){
+//      var input=document.getElementById('bg');
+//      input.style.backgroundColor='white';
+
+// }
+// function changePic(){
+//      var input=document.getElementById('img');
+//      input.src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRl-_UaLq5lyMjH41JO6frMBcZLXJE5DoVXSA&s';
+
+// }
