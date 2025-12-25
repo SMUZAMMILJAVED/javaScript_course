f89d00f7bcbac55db5d0f143718da8206f454683
S MuzammiL Javed <121771009+SMUZAMMILJAVED@users.noreply.github.com>
2025-12-26 02:30:39 +0500

    from chapter 51 to 60

---
+++ class_08/app.js
@@ -0,0 +1,53 @@
+// function expand(){
+//     var text=document.getElementById('p1');
+//     text.innerHTML="Lorem ipsum dolor <b>sit</b> amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet."
+
+// }
+// function hide(){
+// var img=document.getElementById('img');
+// if(img.style.visibility==='hidden'){
+//     img.style.visibility='visible'
+// }
+// else{
+
+
+// img.style.visibility='hidden'}
+// }
+// function change(){
+//     var img=document.getElementById('img');
+//     var img1='https://pid.gov.pk/pid_beta/admin/uploads/profile/1.jpg';
+//     var img2='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4aQ-hFjz8xpV4LoSNkjhe_u_duxxeEzoECg&s';
+//    if( img.src==='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4aQ-hFjz8xpV4LoSNkjhe_u_duxxeEzoECg&s')
+//     {
+//     img.src='https://pid.gov.pk/pid_beta/admin/uploads/profile/1.jpg'
+//    }else{
+//     img.src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4aQ-hFjz8xpV4LoSNkjhe_u_duxxeEzoECg&s'
+//    }
+// }
+// function addBorder(){
+//      var p=document.getElementById('p2');
+//      p.className+=' border'
+// }
+// function change(){
+// var p=document.getElementsByTagName('p');
+// for(var i=0;i<p.length;i++){
+//     p[i].innerHTML="hello world!";
+// }
+
+
+// }
+
+
+// function change(){
+//     var result=document.getElementById('container')
+//    var p=result.getElementsByTagName('p');
+// for(var i=0;i<p.length;i++){
+//     p[i].innerHTML="hello world!";
+// }
+// }
+
+
+// function change(){
+//     var p=document.childNodes[1].childNodes[2].childNodes[11]
+//     p.innerHTML='Hello World!'
+// }
